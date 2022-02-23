77105f92c4fcb75e648fa523584f8a1078d1989c
dindac <dindacahya001@gmail.com>
2022-02-24 01:28:43 +0700

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".your-class").slick({});
+});
+// $(document).ready(function () {
+//   $(".slider").slick({
+//     autoplay: true,
+//     autoplaySpeed: 2500,
+//   });
+// });
